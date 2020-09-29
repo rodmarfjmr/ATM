fd749fd8fca3856e86ab4f8c1132a007d28c3ca3
Fredy Marcos <fredy.marcos@MacBook-FM.local>
2020-09-29 16:49:43 -0600

    CREACION DE PROYECTO

---
+++ PROJECT_ATM.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PROJECT_ATM.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
---
+++ PROJECT_ATM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="PROJECT_ATM.Global" %>
